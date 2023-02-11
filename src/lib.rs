1ff6a18355acedf6f1f666161b3f72dc3fc419db
s3gf4u17 <szymon.kolakowski@gmail.com>
2023-02-11 21:07:16 +0100

    plain based dems

--- src/lib.rs
+++ src/lib.rs
@@ -29,17 +29,25 @@ pub fn sphere(a:f64,b:f64,hmax:f64,file:&str) -> Vec<(f64,f64,f64)> {
         let x = (n+*h)*libm::cos(*phi)*libm::cos(*lambda);
         let y = (n+*h)*libm::cos(*phi)*libm::sin(*lambda);
         let z = (n*(1.0-e2)+*h)*libm::sin(*phi);
-        coordinates[i]=(x,y,z);
+        coordinates[i]=(y,z,x);
     }
     coordinates
 } // hirvonen algorithm used to transform (phi lambda h) to (x y z) coordinates
 
 pub fn plain(deltah:f64,file:&str) -> Vec<(f64,f64,f64)> {
-    Vec::<(f64,f64,f64)>::new()
+    let raster = image::open(file).unwrap();
+    let (raster_width,raster_height) = raster.dimensions();
+    let mut coordinates : Vec<(f64,f64,f64)> = Vec::new();
+    for i in 0..raster_width*raster_height {
+        let pixel = raster.get_pixel(i%raster_width,i/raster_width);
+        let h = (pixel[0] as f64 +pixel[1] as f64 +pixel[2] as f64)/3.0/255.0*deltah;
+        coordinates.push(((i%raster_width).into(),h,(i/raster_width).into()))
+    }
+    coordinates
 }
 
 pub fn export(coordinates:Vec<(f64,f64,f64)>) {
-    let (raster_height,raster_width) = (512,1024);
+    let (raster_height,raster_width) = (300,300);
     println!("creating mesh");
     let mut file = File::create("topography.obj").expect("failed");
     let mut contents = String::new();
@@ -48,7 +56,7 @@ pub fn export(coordinates:Vec<(f64,f64,f64)>) {
     println!("writing vertices");
     for i in 0..coordinates.len() {
         let (x,y,z) = coordinates.get(i).unwrap();
-        contents.push_str(&format!("v {} {} {}\n",y,z,x)as &str);
+        contents.push_str(&format!("v {} {} {}\n",x,y,z)as &str);
     }
     println!("writing faces");
     // fill with faces
@@ -57,10 +65,10 @@ pub fn export(coordinates:Vec<(f64,f64,f64)>) {
             contents.push_str(&format!("f {} {} {} {}\n",raster_width*(y-1)+x,raster_width*(y-1)+x+1,raster_width*y+x+1,raster_width*y+x) as &str);
         }
     } // at this point we always have an unconnected space on meridian 180
-    println!("fixing meridian 180");
-    for y in 1..raster_height {
-        contents.push_str(&format!("f {} {} {} {}\n",raster_width*y,raster_width*(y-1)+1,raster_width*(y)+1,raster_width*(y+1)) as &str);
-    }
+    // println!("fixing meridian 180");
+    // for y in 1..raster_height {
+    //     contents.push_str(&format!("f {} {} {} {}\n",raster_width*y,raster_width*(y-1)+1,raster_width*(y)+1,raster_width*(y+1)) as &str);
+    // }
     println!("saving mesh");
     file.write_all(contents.as_bytes()).expect("failed");
 }
