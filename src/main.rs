c70e02cc281c0310c8f51b296719b0aee36573c6
s3gf4u17 <szymon.kolakowski@gmail.com>
2023-02-11 14:31:58 +0100

    arguments parser

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,43 @@
-use image::GenericImageView;
-use image::GenericImage;
-use image::Rgba;
-use image::RgbaImage;
-use image::ImageBuffer;
 mod lib;
 
-use std::fs::File;
-use std::io::Write;
-
 fn main() {
-    let coordinates = lib::sphere(3396190.0,3376200.0,"test/mars.jpg");
-    println!("{:?}",coordinates[coordinates.len()-1]);
+    let mut argumentpointer = 1;
+
+    let coordinates = match &std::env::args().nth(argumentpointer).expect("no base shape given") as &str {
+        "--sphere"=>{
+            argumentpointer+=1;
+            let r = (&std::env::args().nth(argumentpointer).expect("no radius given") as &str).parse::<f64>().unwrap();
+            argumentpointer+=1;
+            let hmax = (&std::env::args().nth(argumentpointer).expect("no max height given") as &str).parse::<f64>().unwrap();
+            argumentpointer+=1;
+            let file = &std::env::args().nth(argumentpointer).expect("no input file given") as &str;
+            lib::sphere(r,r,hmax,file)
+        },
+        "--ellipsoid"=>{
+            argumentpointer+=1;
+            let a = (&std::env::args().nth(argumentpointer).expect("no radius given") as &str).parse::<f64>().unwrap();
+            argumentpointer+=1;
+            let b = (&std::env::args().nth(argumentpointer).expect("no radius given") as &str).parse::<f64>().unwrap();
+            argumentpointer+=1;
+            let hmax = (&std::env::args().nth(argumentpointer).expect("no max height given") as &str).parse::<f64>().unwrap();
+            argumentpointer+=1;
+            let file = &std::env::args().nth(argumentpointer).expect("no input file given") as &str;
+            lib::sphere(a,b,hmax,file)
+        },
+        "--plain"=>{
+            argumentpointer+=1;
+            let deltah = (&std::env::args().nth(argumentpointer).expect("no max height given") as &str).parse::<f64>().unwrap();
+            argumentpointer+=1;
+            let file = &std::env::args().nth(argumentpointer).expect("no input file given") as &str;
+            lib::plain(deltah,file)
+        },
+        _=>panic!("non existing base shape"),
+    };
+
+    argumentpointer+=1;
+    match &std::env::args().nth(argumentpointer).expect("no action given") as &str {
+        "--visualize"=>println!("visualize"),
+        "--export"=>println!("export"),
+        _=>panic!("non existing action"),
+    }
 }
