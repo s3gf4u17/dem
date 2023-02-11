beb7d7ea73ca03fa7f80878357b2be18c7842f57
s3gf4u17 <szymon.kolakowski@gmail.com>
2023-02-11 14:32:18 +0100

    plain based obj generator prototype

--- src/lib.rs
+++ src/lib.rs
@@ -4,7 +4,7 @@ use libm;
 
 // INPUT - filename && elipsoid info && max height
 // OUTPUT - vector of xyz coordinates
-pub fn sphere(a:f64,b:f64,file:&str) -> Vec<(f64,f64,f64)> {
+pub fn sphere(a:f64,b:f64,hmax:f64,file:&str) -> Vec<(f64,f64,f64)> {
     // calculate constant eccentricity of the ellipsoid
     let e2 = (a*a-b*b)/(a*a);
     // read raster input file and get its size
@@ -17,17 +17,21 @@ pub fn sphere(a:f64,b:f64,file:&str) -> Vec<(f64,f64,f64)> {
         let phi = PI/2.0-(PI/raster_height as f64)*(i as f64 / raster_width as f64);
         let lambda = -PI+(2.0*PI/raster_width as f64)*(i as f64 % raster_width as f64);
         let pixel = raster.get_pixel(i%raster_width,i/raster_width);
-        let h = (pixel[0] as f64 +pixel[1] as f64 +pixel[2] as f64)/3.0;
+        let h = (pixel[0] as f64 +pixel[1] as f64 +pixel[2] as f64)/3.0/255.0*hmax;
         coordinates.push((phi,lambda,h));
     }
     // transform coordinates to (x y z)
     for i in 0..coordinates.len() {
         let (phi,lambda,h) = coordinates.get(i).unwrap();
-        let n = a/libm::sqrt(1.0-e2*libm::sin(*lambda)*libm::sin(*lambda));
-        let x = (n+*h)*libm::cos(*lambda)*libm::cos(*phi);
-        let y = (n+*h)*libm::cos(*lambda)*libm::sin(*phi);
-        let z = (n*(1.0-e2)+*h)*libm::sin(*lambda);
+        let n = a/libm::sqrt(1.0-e2*libm::sin(*phi)*libm::sin(*phi));
+        let x = (n+*h)*libm::cos(*phi)*libm::cos(*lambda);
+        let y = (n+*h)*libm::cos(*phi)*libm::sin(*lambda);
+        let z = (n*(1.0-e2)+*h)*libm::sin(*phi);
         coordinates[i]=(x,y,z);
     }
     coordinates
 } // hirvonen algorithm used to transform (phi lambda h) to (x y z) coordinates
+
+pub fn plain(deltah:f64,file:&str) -> Vec<(f64,f64,f64)> {
+    Vec::<(f64,f64,f64)>::new()
+}
