89ec0b1402d0639300eb31ef6090f40c2587c183
s3gf4u17 <szymon.kolakowski@gmail.com>
2023-02-18 14:41:55 +0100

    three base export types

--- src/main.rs
+++ src/main.rs
@@ -1,68 +1,46 @@
 mod lib;
-use std::net::TcpListener;
-use std::net::TcpStream;
-use std::io::BufReader;
-use std::io::prelude::*;
-
-fn handle_connection(mut stream:TcpStream) {
-    let http_request : Vec<_> = BufReader::new(&mut stream).lines().map(|result|result.unwrap()).take_while(|line|!line.is_empty()).collect();
-    let (status,content) = match &http_request[0] as &str {
-        "GET / HTTP/1.1" => ("HTTP/1.1 200 OK",include_str!("index.html")),
-        "GET /script.js HTTP/1.1" => ("HTTP/1.1 200 OK",include_str!("script.js")),
-        _=>("HTTP/1.1 404 NOT FOUND","404 page not found"),
-    };
-    let response = format!("{}\r\nContent-Length:{}\r\n\r\n{}",status,content.len(),content);
-    stream.write_all(response.as_bytes()).unwrap();
-}
 
 fn main() {
-    let listener = TcpListener::bind("127.0.0.1:8000").unwrap();
+    let mut argumentpointer = 1;
+    let mut issphere = false;
+    let mut width = 0;
+    let mut height = 0;
 
-    for stream in listener.incoming() {
-        let stream = stream.unwrap();
-        handle_connection(stream);
-    }
+    let coordinates = match &std::env::args().nth(argumentpointer).expect("no base shape given") as &str {
+        "--sphere"=>{
+            issphere = true;
+            argumentpointer+=1;
+            let r = (&std::env::args().nth(argumentpointer).expect("no radius given") as &str).parse::<f64>().unwrap();
+            argumentpointer+=1;
+            let hmax = (&std::env::args().nth(argumentpointer).expect("no max height given") as &str).parse::<f64>().unwrap();
+            argumentpointer+=1;
+            let file = &std::env::args().nth(argumentpointer).expect("no input file given") as &str;
+            let (c,w,h) = lib::sphere(r,r,hmax,file);
+            width = w; height = h; c
+        },
+        "--ellipsoid"=>{
+            issphere = true;
+            argumentpointer+=1;
+            let a = (&std::env::args().nth(argumentpointer).expect("no radius given") as &str).parse::<f64>().unwrap();
+            argumentpointer+=1;
+            let b = (&std::env::args().nth(argumentpointer).expect("no radius given") as &str).parse::<f64>().unwrap();
+            argumentpointer+=1;
+            let hmax = (&std::env::args().nth(argumentpointer).expect("no max height given") as &str).parse::<f64>().unwrap();
+            argumentpointer+=1;
+            let file = &std::env::args().nth(argumentpointer).expect("no input file given") as &str;
+            let (c,w,h) = lib::sphere(a,b,hmax,file);
+            width = w; height = h; c
+        },
+        "--plain"=>{
+            argumentpointer+=1;
+            let deltah = (&std::env::args().nth(argumentpointer).expect("no max height given") as &str).parse::<f64>().unwrap();
+            argumentpointer+=1;
+            let file = &std::env::args().nth(argumentpointer).expect("no input file given") as &str;
+            let (c,w,h) = lib::plain(deltah,file);
+            width = w; height = h; c
+        },
+        _=>panic!("non existing base shape"),
+    };
 
-    // let mut argumentpointer = 1;
-    // let mut issphere = false;
-    // let mut width = 0;
-    // let mut height = 0;
-
-    // let coordinates = match &std::env::args().nth(argumentpointer).expect("no base shape given") as &str {
-    //     "--sphere"=>{
-    //         issphere = true;
-    //         argumentpointer+=1;
-    //         let r = (&std::env::args().nth(argumentpointer).expect("no radius given") as &str).parse::<f64>().unwrap();
-    //         argumentpointer+=1;
-    //         let hmax = (&std::env::args().nth(argumentpointer).expect("no max height given") as &str).parse::<f64>().unwrap();
-    //         argumentpointer+=1;
-    //         let file = &std::env::args().nth(argumentpointer).expect("no input file given") as &str;
-    //         let (c,w,h) = lib::sphere(r,r,hmax,file);
-    //         width = w; height = h; c
-    //     },
-    //     "--ellipsoid"=>{
-    //         issphere = true;
-    //         argumentpointer+=1;
-    //         let a = (&std::env::args().nth(argumentpointer).expect("no radius given") as &str).parse::<f64>().unwrap();
-    //         argumentpointer+=1;
-    //         let b = (&std::env::args().nth(argumentpointer).expect("no radius given") as &str).parse::<f64>().unwrap();
-    //         argumentpointer+=1;
-    //         let hmax = (&std::env::args().nth(argumentpointer).expect("no max height given") as &str).parse::<f64>().unwrap();
-    //         argumentpointer+=1;
-    //         let file = &std::env::args().nth(argumentpointer).expect("no input file given") as &str;
-    //         let (c,w,h) = lib::sphere(a,b,hmax,file);
-    //         width = w; height = h; c
-    //     },
-    //     "--plain"=>{
-    //         argumentpointer+=1;
-    //         let deltah = (&std::env::args().nth(argumentpointer).expect("no max height given") as &str).parse::<f64>().unwrap();
-    //         argumentpointer+=1;
-    //         let file = &std::env::args().nth(argumentpointer).expect("no input file given") as &str;
-    //         let (c,w,h) = lib::plain(deltah,file);
-    //         width = w; height = h; c
-    //     },
-    //     _=>panic!("non existing base shape"),
-    // };
-
-    // lib::export(coordinates,issphere,height,width);
+    lib::export(coordinates,issphere,height,width);
 }
